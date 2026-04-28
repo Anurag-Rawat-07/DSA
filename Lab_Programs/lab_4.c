e64190ae337b88de8482e621c4ebf06df1a3c746
Anurag Rawat <246465749+anurag-rawat-hub@users.noreply.github.com>
2026-04-28 23:31:06 +0530

    edited double ended queue

--- Lab_Programs/lab_4.c
+++ Lab_Programs/lab_4.c
@@ -39,10 +39,37 @@ void display(){
     }
 }
 
+void insert_from_front(int x){
+    if(f==0 && r==-1){
+        printf("Element inserted from front\n");
+        r++;
+        q[r]=x;
+    }
+    else if(f!=0){
+        printf("Element inserted from front\n");
+        f--;
+        q[f]=x;
+    }
+    else{
+        printf("Cannot insert from front\n");
+    }
+}
+
+void delete_from_rear(){
+    if(f>r){
+        printf("Queue is empty\n");
+    }
+    else{
+        printf("Deleted element from rear is %d\n",q[r]);
+        r--;
+    }
+}
+
 int main(){
     int choice,x;
     while(1){
-        printf("1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\n");
+        printf("\n***Queue Operations***\n");
+        printf("1.Enqueue rear\n2.Dequeue front\n3.Enqueue front\n4.Dequeue rear\n5.Display\n6.Exit");
         printf("Enter your choice: ");
         scanf("%d",&choice);
         switch(choice){
@@ -55,9 +82,17 @@ int main(){
                 dequeue();
                 break;
             case 3:
-                display();
+                printf("Enter the element to be enqueued: ");
+                scanf("%d",&x);
+                insert_from_front(x);
                 break;
             case 4:
+                delete_from_rear();
+                break;
+            case 5:
+                display();
+                break;
+            case 6:
                 return 0;
             default:
                 printf("Invalid choice\n");
