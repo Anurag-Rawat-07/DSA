f12822cffaa9a9958cd3345368382f6603ae534f
Anurag Rawat <246465749+anurag-rawat-hub@users.noreply.github.com>
2026-05-02 23:55:41 +0530

    pattern matching added

---
+++ Extra_Programs/pattern_matching.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], pattern[100];
+    printf("Enter the string: ");
+    scanf("%s", str);
+    printf("Enter the pattern: ");
+    scanf("%s", pattern);
+    
+    int str_len = strlen(str);
+    int pat_len = strlen(pattern);
+    
+    for (int i = 0; i <= str_len - pat_len; i++) {
+        int j;
+        for (j = 0; j < pat_len; j++) {
+            if (str[i + j] != pattern[j]) {
+                break;
+            }
+        }
+        if (j == pat_len) {
+            printf("Pattern found at index %d\n", i);
+        }
+    }
+    
+    return 0;
+}
