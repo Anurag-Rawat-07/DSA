b3cc755a5a1695e81acc4fa254780663f5974f9f
Anurag Rawat <246465749+anurag-rawat-hub@users.noreply.github.com>
2026-08-08 11:56:25 +0530

    pallindrome

---
+++ LeetCode/9.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0)){
+            return false;
+        }
+        unsigned int reverse=0;
+        int original=x;
+
+        while(x!=0){
+            reverse=(reverse*10)+(x%10);
+            x=x/10;
+        }
+        if(reverse==original){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
