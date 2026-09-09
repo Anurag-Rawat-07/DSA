8109c488ced682f4f80b809b81de313f86732086
Anurag Rawat <246465749+Anurag-Rawat-07@users.noreply.github.com>
2026-09-09 12:13:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.72%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int cnt=0;
         if(n<1000){
             return 0;
         }
-        for(int i=1000; i<=n; i++){
-            cnt++;
-        }
-        return cnt;
+        return n-999;
     }
 };
