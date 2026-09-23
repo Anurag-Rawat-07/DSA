ea3f51f21f92e1c2e0a0514d654e2e664fe2b239
Anurag Rawat <246465749+Anurag-Rawat-07@users.noreply.github.com>
2026-09-23 23:32:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (2.91%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        string res="";
+        int cnt=0;
+        while(n!=1){
+            if(n%2==1){
+                res+='1';
+                cnt++;
+            }
+            else{
+                res+='0';
+            }
+            n=n/2;
+        }
+        return cnt+1;
+        
+    }
+};
